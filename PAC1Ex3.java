fc63aa72b2f83b363281056745b77ae2a11bb40a
DESKTOP-AE16OM0\GuillofinPofin <guillerneira@gmail.com>
2025-02-28 19:50:43 +0100

    method checkWinDiagonal

--- PAC1Ex3.java
+++ PAC1Ex3.java
@@ -8,8 +8,8 @@ public class PAC1Ex3 {
     public static boolean isValidColumn(char[][] board, int column) {
         //TODO
         boolean validColumn=false;
-        if (column > 0 || column <=MAX_COLUMNS){
-            for(int row=MAX_ROWS; row >= 0; row--) {
+        if (column > 0 || column < MAX_COLUMNS){
+            for(int row = MAX_ROWS - 1; row >= 0; row--) {
                 if(board[row][column]==' '){
                    validColumn=true;
                    break;
@@ -46,9 +46,9 @@ public class PAC1Ex3 {
         int flag=0;
         char previous = color;
         do {
-            for (int row = 0; row <= MAX_ROWS; row++) {
+            for (int row = 0; row < MAX_ROWS; row++) {
                 flag = 0;
-                for (int column = 0; column <= MAX_COLUMNS; column++) {
+                for (int column = 0; column < MAX_COLUMNS; column++) {
                     previous=board[row][column];
                     if (previous == color) {
                         flag++;
@@ -70,9 +70,9 @@ public class PAC1Ex3 {
         int flag=0;
         char previous = color;
         do {
-            for (int row = 0; row <= MAX_ROWS; row++) {
+            for (int column = 0; column < MAX_COLUMNS; column++) {
                 flag = 0;
-                for (int column = 0; column <= MAX_COLUMNS; column++) {
+                for (int row = 0; row < MAX_ROWS; row++) {
                     previous=board[row][column];
                     if (previous == color) {
                         flag++;
@@ -91,6 +91,78 @@ public class PAC1Ex3 {
         //TODO
         boolean winD =false;
         int flag=0;
+        int row;
+        int column;
+        char previous=' ';
+        //First square
+        for (row=0; row<3; row++) {
+            column=0;
+            do {
+                previous = board[row][column];
+                row++;
+                column++;
+                if (previous == color) {
+                    flag++;
+                } else {
+                    flag = 0;
+                }
+            } while (row < MAX_ROWS);
+        }
+        if (flag==4){
+            winD=true;
+        }
+        //Second Square
+        for (row=MAX_ROWS-1; row<=0; row--) {
+            column=0;
+            do {
+                previous = board[row][column];
+                row--;
+                column++;
+                if (previous == color) {
+                    flag++;
+                } else {
+                    flag = 0;
+                }
+            } while (row >=0);
+        }
+        if (flag==4){
+            winD=true;
+        }
+        //Third square
+        for (row=0; row<3; row++) {
+            column=MAX_COLUMNS-1;
+            do {
+                previous = board[row][column];
+                row++;
+                column--;
+                if (previous == color) {
+                    flag++;
+                } else {
+                    flag = 0;
+                }
+            } while (row < MAX_ROWS);
+
+        }
+        if (flag==4){
+            winD=true;
+        }
+        //Fourth Square
+        for (row=MAX_ROWS-1; row<=0; row--) {
+            column=MAX_COLUMNS-1;
+            do {
+                previous = board[row][column];
+                row--;
+                column--;
+                if (previous == color) {
+                    flag++;
+                } else {
+                    flag = 0;
+                }
+            } while (row >=0);
+        }
+        if (flag==4){
+            winD=true;
+        }
 
 
         return winD;
@@ -98,6 +170,7 @@ public class PAC1Ex3 {
 
     public static boolean checkWin(char[][] board, char color) {
         //TODO
+        
     }
 
     public static char[][] dropPiece(char[][] board, int column, char color) {
